2b84ad77241c0750c9799d20a51b14ad0512ac05
smerca <sergi.mercadelaborda@gmail.com>
2018-10-07 19:25:02 +0200

    Added simultaneous client and server on esp32 core 1

--- mainmultiloop/mainmultiloop.ino
+++ mainmultiloop/mainmultiloop.ino
@@ -38,6 +38,7 @@ void serverTask2( void * parameter){
 
   //Setup
   Serial.begin(115200);
+  pinMode(5, OUTPUT);      // set the LED pin mode
   server.begin();
   Serial.print("server is at ");
   Serial.println(WiFi.localIP());
@@ -47,7 +48,51 @@ void serverTask2( void * parameter){
     WiFiClient client = server.available();
     if (client) {
         Serial.println("new client");
+        String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
+
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(5, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(5, LOW);                // GET /L turns the LED off
+        }
+      }
     }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+    }
+  }
 }
 
 ///SETUP
@@ -75,7 +120,7 @@ void setup(){
   xTaskCreatePinnedToCore(
     serverTask2,             //Task function
     "Task_2",                 //name of task
-    1000,                     //Stack size of the task
+    5000,                     //Stack size of the task
     NULL,                     //parameter of the task
     1,                        //priority of the task
     &Task2,                   //Task handle to keep track of created task
